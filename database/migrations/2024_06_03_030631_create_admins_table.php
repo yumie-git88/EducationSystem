7e75b79c7eb9e0f2b5058fa81120a33a7c8c0792
ayaka <ayaka.hashimoto00@gmail.com>
2024-06-05 18:44:27 +0900

    Add 4

---
+++ database/migrations/2024_06_03_030631_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255); // タイトル
+            $table->dateTime('posted_date'); // 投稿日時
+            $table->longText('article_contents'); // 記事内容
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
+
