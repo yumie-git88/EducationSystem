9be171e668e7c5623bd89ef53151518ece534954
naito-hiroaki <naitoh010@gmail.com>
2024-06-06 21:08:03 +0900

    Add 1 grades modify

--- database/migrations/2024_05_31_152602_create_grades_table.php
+++ database/migrations/2024_05_31_152602_create_grades_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 255);
             $table->timestamps();
         });
 
