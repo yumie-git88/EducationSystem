638bfbe80025924fb89d1eb4fdc3f0a4d1961f40
naito-hiroaki <naitoh010@gmail.com>
2024-07-09 20:40:37 +0900

    ユーザーと管理者のダミーデータ作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
+            AdminSeeder::class,
             ArticleSeeder::class,
             CurriculumSeeder::class,
             CurriculumProgressSeeder::class,
