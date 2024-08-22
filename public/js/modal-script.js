36d72ff4b4d03e1c6a4895c8c3e587d4f7c79b72
kobayashi <arro.8w8@gmail.com>
2024-08-23 00:16:32 +0900

    13 Commit/モーダル修正

--- public/js/modal-script.js
+++ public/js/modal-script.js
@@ -1,7 +1,5 @@
 $(function() { //モーダル表示
   $('#registerModal').on('show.bs.modal', function (e) {
-    let isValid = validateForm();
-    if (isValid) {
       let name = $('#name').val();
       let name_kana = $('#name_kana').val();
       let email = $('#email').val();
@@ -11,23 +9,5 @@ $(function() { //モーダル表示
       modal.find('#modalKana').text(name_kana);
       modal.find('#modalEmail').text(email);
       // modal.find('#modalPassword').text(password);
-      console.log('バリデーション成功');
-    } else {
-      console.log('バリデーション失敗');
-    }
   });
 });
-
-function validateForm(e) { //バリデーション
-  if(document.getElementById("name").value === "") {
-    return false;
-  } else if(document.getElementById("name_kana").value === "") {
-    return false;
-  } else if(document.getElementById("email").value === "") {
-    return false;
-  } else if(document.getElementById("password").value === "") {
-    return false;
-  } else {
-    return true; // すべてのチェックがパスした場合は true を返す
-  }
-}
